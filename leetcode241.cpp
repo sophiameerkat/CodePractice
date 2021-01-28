59ca71f74e03cc1d1e7beed37fe28d89cbaae512
Sophia Huang <b07902031@csie.ntu.edu.tw>
2021-01-28 13:48:31 +0800

    change some code for problem 241

--- leetcode241.cpp
+++ leetcode241.cpp
@@ -15,8 +15,8 @@ public:
         for (int i = 0; i < input.size(); i++) {
             char op = input[i];
             if (op == '+' || op == '-' || op == '*') {
-                auto left = diffWaysToCompute(input.substr(0, i));
-                auto right = diffWaysToCompute(input.substr(i+1));
+                vector<int> left = diffWaysToCompute(input.substr(0, i));
+                vector<int> right = diffWaysToCompute(input.substr(i+1));
                 for (int l: left) {
                     for (int r: right) {
                         if (op == '+')
